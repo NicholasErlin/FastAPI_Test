abee7f2d225a9942f7ec45d13de953071110e406
Nicholas <nicholaserlin@gmail.com>
2023-09-21 05:58:44 +0200

    changed Student to AirData

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import os
 from fastapi import FastAPI, Body, HTTPException, status
 from fastapi.responses import Response, JSONResponse
@@ -8,8 +9,8 @@ from typing import Optional, List
 import motor.motor_asyncio
 
 app = FastAPI()
-client = motor.motor_asyncio.AsyncIOMotorClient("")
-db = client.college
+client = motor.motor_asyncio.AsyncIOMotorClient("mongodb+srv://admin:admin@clustertest.33x3iom.mongodb.net/test?retryWrites=true&w=majority")
+db = client.core2data
 
 
 class PyObjectId(ObjectId):
@@ -28,96 +29,92 @@ class PyObjectId(ObjectId):
         field_schema.update(type="string")
 
 
-class StudentModel(BaseModel):
+class AirDataModel(BaseModel):
     id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
-    name: str = Field(...)
-    email: EmailStr = Field(...)
-    course: str = Field(...)
-    gpa: float = Field(..., le=4.0)
-
+    eco2: int = Field(...)
+    h2: int = Field(...)
+    ethanol: int = Field(...)
+    timestamp: datetime = Field(default_factory=datetime.utcnow)
     class Config:
         allow_population_by_field_name = True
         arbitrary_types_allowed = True
         json_encoders = {ObjectId: str}
         schema_extra = {
             "example": {
-                "name": "Jane Doe",
-                "email": "jdoe@example.com",
-                "course": "Experiments, Science, and Fashion in Nanophotonics",
-                "gpa": "3.0",
+                "eco2": 2,
+                "h2": 3,
+                "ethanol": 7
             }
         }
 
 
-class UpdateStudentModel(BaseModel):
-    name: Optional[str]
-    email: Optional[EmailStr]
-    course: Optional[str]
-    gpa: Optional[float]
+class UpdateAirDataModel(BaseModel):
+    eco2: Optional[int]
+    h2: Optional[int]
+    ethanol: Optional[int]
 
     class Config:
         arbitrary_types_allowed = True
         json_encoders = {ObjectId: str}
         schema_extra = {
             "example": {
-                "name": "Jane Doe",
-                "email": "jdoe@example.com",
-                "course": "Experiments, Science, and Fashion in Nanophotonics",
-                "gpa": "3.0",
+                "eco2": 2,
+                "h2": 3,
+                "ethanol": 7
             }
         }
 
 
-@app.post("/", response_description="Add new student", response_model=StudentModel)
-async def create_student(student: StudentModel = Body(...)):
-    student = jsonable_encoder(student)
-    new_student = await db["students"].insert_one(student)
-    created_student = await db["students"].find_one({"_id": new_student.inserted_id})
-    return JSONResponse(status_code=status.HTTP_201_CREATED, content=created_student)
+@app.post("/", response_description="Add new Air Data Value", response_model=AirDataModel)
+async def create_airdata(airData: AirDataModel = Body(...)):
+    airData = jsonable_encoder(airData)
+    new_airData = await db["airData"].insert_one(airData)
+    created_airData = await db["airData"].find_one({"_id": new_airData.inserted_id})
+    return JSONResponse(status_code=status.HTTP_201_CREATED, content=created_airData)
 
 
 @app.get(
-    "/", response_description="List all students", response_model=List[StudentModel]
+    "/", response_description="List all Air Data Values", response_model=List[AirDataModel]
 )
-async def list_students():
-    students = await db["students"].find().to_list(1000)
-    return students
+async def list_airData():
+    airData = await db["airData"].find().to_list(1000)
+    return airData
 
 
 @app.get(
-    "/{id}", response_description="Get a single student", response_model=StudentModel
+    "/{id}", response_description="Get a single Air Data by Id", response_model=AirDataModel
 )
-async def show_student(id: str):
-    if (student := await db["students"].find_one({"_id": id})) is not None:
-        return student
+async def show_airData(id: str):
+    if (airData := await db["airData"].find_one({"_id": id})) is not None:
+        return airData
 
-    raise HTTPException(status_code=404, detail=f"Student {id} not found")
+    raise HTTPException(status_code=404, detail=f"Air Data {id} not found")
 
 
-@app.put("/{id}", response_description="Update a student", response_model=StudentModel)
-async def update_student(id: str, student: UpdateStudentModel = Body(...)):
-    student = {k: v for k, v in student.dict().items() if v is not None}
+@app.put("/{id}", response_description="Update an Air Data Value", response_model=AirDataModel)
+async def update_airData(id: str, airData: UpdateAirDataModel = Body(...)):
+    airData = {k: v for k, v in airData.dict().items() if v is not None}
 
-    if len(student) >= 1:
-        update_result = await db["students"].update_one({"_id": id}, {"$set": student})
+    if len(airData) >= 1:
+        update_result = await db["airData"].update_one({"_id": id}, {"$set": airData})
 
         if update_result.modified_count == 1:
             if (
-                updated_student := await db["students"].find_one({"_id": id})
+                updated_airData := await db["airData"].find_one({"_id": id})
             ) is not None:
-                return updated_student
+                return updated_airData
 
-    if (existing_student := await db["students"].find_one({"_id": id})) is not None:
-        return existing_student
+    if (existing_airData := await db["airData"].find_one({"_id": id})) is not None:
+        return existing_airData
 
-    raise HTTPException(status_code=404, detail=f"Student {id} not found")
+    raise HTTPException(status_code=404, detail=f"Air Data Value {id} not found")
 
 
-@app.delete("/{id}", response_description="Delete a student")
-async def delete_student(id: str):
-    delete_result = await db["students"].delete_one({"_id": id})
+@app.delete("/{id}", response_description="Delete an Air Data Value")
+async def delete_airData(id: str):
+    delete_result = await db["airData"].delete_one({"_id": id})
 
     if delete_result.deleted_count == 1:
         return Response(status_code=status.HTTP_204_NO_CONTENT)
 
-    raise HTTPException(status_code=404, detail=f"Student {id} not found")
+    raise HTTPException(status_code=404, detail=f"Air Data Value {id} not found")
